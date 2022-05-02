e95994a65f6a2d8a2f9c716976187b671c5fb9a8
Andrés Parra <andresparra003@gmail.com>
2022-05-02 09:24:34 +0200

    Created wallArray, gameObjects and gameRules with access methods on GameScreen class

--- Game/src/main/java/Model/GameScreen.java
+++ Game/src/main/java/Model/GameScreen.java
@@ -1,14 +1,15 @@
 package Model;
 
+import java.util.ArrayList;
+
 public abstract class GameScreen {
 
     private int width;
     private int height;
     private long gameStartMillis;
-    Wall wall1;
-    Wall wall2;
-    Wall wall3;
-    Wall wall4;
+    private Wall[] wallArray = {new Wall(), new Wall(), new Wall(), new Wall()};
+    private ArrayList<GameObject> gameObjects = new ArrayList<>();
+    private GameRules gameRules;
 
     public int getWidth() {
         return width;
@@ -34,7 +35,49 @@ public abstract class GameScreen {
         this.gameStartMillis = gameStartMillis;
     }
 
-    public void update(){}
+    public Wall[] getWallArray() {
+        return wallArray;
+    }
+
+    public void setWallArray(Wall[] wallArray) {
+        this.wallArray = wallArray;
+    }
+
+    public ArrayList<GameObject> getGameObjects() {
+        return gameObjects;
+    }
+
+    public void setGameObjects(ArrayList<GameObject> gameObjects) {
+        this.gameObjects = gameObjects;
+    }
+
+    public GameRules getGameRules() {
+        return gameRules;
+    }
+
+    public void setGameRules(GameRules gameRules) {
+        this.gameRules = gameRules;
+    }
+
+    public void addGameObject(GameObject gameObject) {
+        gameObjects.add(gameObject);
+    }
+
+    public boolean removeGameObject(GameObject gameObject) {
+        return gameObjects.remove(gameObject);
+    }
+
+    public Wall getWall(int index) {
+        if (index < 0 || index > 3) {
+            System.err.println("Out of bounds access on wallArray with index: " + index);
+            return null;
+        } else {
+            return this.wallArray[index];
+        }
+    }
+
+    public void update() {
+    }
 
     public GameObject addObject() {
         return null;
