ad5788ae6da149e2ba2237f5a31771670e32c636
AlfonsLD <alfonslorentedarder@gmail.com>
2022-05-19 20:39:37 +0200

    Physics commit

--- Game/src/main/java/Model/DynamicBody.java
+++ Game/src/main/java/Model/DynamicBody.java
@@ -1,4 +1,152 @@
 package Model;
 
-public class DynamicBody {
+public class DynamicBody extends Body{
+    
+    private float speedX, speedY;
+    private float frictionCofficient = 0.05f;
+
+    //TO DO 
+    
+    //implement
+    float speedLimit = 5;
+    
+    
+    //create
+    //private float[] descomponerFuerzas( angulo , fuerza ) { return [ velX, velY ] }
+    
+    
+    //ESTO ES UN OBJETO MOVIBLE
+    //MAP SIZE --> 1000, 700
+    public DynamicBody() {
+    
+        speedX = (float) Math.random() * 5 - 2.5f;
+        speedY = (float) Math.random() * 5 - 2.5f;
+                
+    }
+    
+    
+    public float getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(float speedX) {
+        this.speedX = speedX;
+    }
+
+    public float getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(float speedY) {
+        this.speedY = speedY;
+    }
+
+    public float getFrictionCofficient() {
+        return frictionCofficient;
+    }
+
+    public void setFrictionCofficient(float friction) {
+        this.frictionCofficient = friction;
+    }
+    
+    public void move( float angulo, float potencia ) {
+
+        //APLICAR FOLMULA PARA SACAR FUERZA DE X e Y - sin cos
+        double anguloRad = Math.toRadians(angulo);
+        float sin = potencia * (float)Math.sin(anguloRad);
+        float cos = potencia * (float)Math.cos(anguloRad);
+        
+        float addSpeedX = sin * 1 / 90;
+        float addSpeedY = cos * -1 / 90;
+        
+        //Aplicar veloidad
+
+        speedY += addSpeedY;
+        speedX += addSpeedX;
+        
+        
+        float speedTotal = (float) Math.sqrt( (speedY*speedY) + (speedX*speedX));
+        
+        if ( speedTotal > speedLimit ) {
+            
+            float xNorm = speedX / speedTotal;
+            float yNorm = speedY / speedTotal;
+            
+            speedX = xNorm * speedLimit;
+            speedY = yNorm * speedLimit;
+            
+        }
+        
+        
+        if(potencia > 0){
+        System.out.println("potencia: " + potencia);        
+        System.out.println("angulo: " + angulo);
+
+        System.out.println("cos: " + cos);
+        System.out.println("sin: " + sin);
+        System.out.println("\n\n");
+        }
+        
+        
+        this.checkBorderCollisions();
+        this.applyFriction();
+        
+        
+    }
+    
+    
+    public void checkBorderCollisions(){
+        //CHECK COLISIONS WITH WALLS
+        float posX = super.getPosX();
+        float posY = super.getPosY();
+        
+        //IF COLISION RIGHT 
+        if( ( posX + super.getRadius() )  > 1270 ) {
+            super.setPosX( super.getPosX() - speedX );
+            speedX = -speedX;
+        //IF COLISION LEFT
+        } else if ( posX - super.getRadius() < 0 ){
+            super.setPosX( super.getPosX() - speedX );
+            speedX = -speedX;
+        
+        //IF NOT COLISION
+        } else {
+            super.setPosX( super.getPosX() + speedX );
+        }
+        
+        
+        //IF COLISION UP
+        if( posY - super.getRadius() < 0 ) {
+            super.setPosY( super.getPosY() - speedY );
+            speedY = -speedY;
+        //IF COLISION BOT
+        } else if ( posY + super.getRadius()  > 950 ){
+            super.setPosY( super.getPosY() - speedY );
+            speedY = -speedY;
+        
+        //IF NOT COLISION
+        } else {
+            super.setPosY( super.getPosY() + speedY );
+        }
+    }
+    
+   
+
+    private void applyFriction() {
+       //SPEED LOSE
+        if ( speedX > 0.05 ) {
+            speedX -=  frictionCofficient;
+        } else if ( speedX < -0.05 ) {
+            speedX += frictionCofficient;
+        } else speedX = 0;
+  
+        if ( speedY > 0.05 ) {
+            speedY -=  frictionCofficient;
+        } else if ( speedY < -0.05 ) {
+            speedY += frictionCofficient;
+        } else speedY = 0;
+    }
+    
+    
+    
 }
--- Game/src/main/java/Model/GameObject.java
+++ Game/src/main/java/Model/GameObject.java
@@ -1,88 +1,33 @@
 package Model;
 
-import View.Animation;
-
-import java.awt.*;
-import java.util.HashMap;
-
 public class GameObject {
-    protected boolean visible;
-    protected float health;
-    protected boolean invincible;
-    protected HashMap<String, Animation> animationMap;
-    protected Animation currentAnimation;
-    protected Body body;
-
-    /*--------------------
-        Getters/Setters
-     --------------------*/
-
-    public boolean isVisible() {
-        return visible;
+    private Body staticBody;
+    private DynamicBody dynamicBody;
+    
+    public GameObject(DynamicBody dynamicBody){
+        this.dynamicBody = dynamicBody;
+    }
+    
+    public GameObject(Body staticBody){
+        this.staticBody = staticBody;
     }
 
-    public void setVisible(boolean visible) {
-        this.visible = visible;
+    public Body getStaticBody() {
+        return staticBody;
     }
 
-    public float getHealth() {
-        return health;
+    public void setStaticBody(Body staticBody) {
+        this.staticBody = staticBody;
     }
 
-    public void setHealth(float health) {
-        this.health = health;
+    public DynamicBody getDynamicBody() {
+        return dynamicBody;
     }
 
-    public boolean isInvincible() {
-        return invincible;
+    public void setDynamicBody(DynamicBody dynamicBody) {
+        this.dynamicBody = dynamicBody;
     }
-
-    public void setInvincible(boolean invincible) {
-        this.invincible = invincible;
-    }
-
-    public HashMap<String, Animation> getAnimationMap() {
-        return animationMap;
-    }
-
-    public void setAnimationMap(HashMap<String, Animation> animationMap) {
-        this.animationMap = animationMap;
-    }
-
-    public Animation getCurrentAnimation() {
-        return currentAnimation;
-    }
-
-    public void setCurrentAnimation(Animation currentAnimation) {
-        this.currentAnimation = currentAnimation;
-    }
-
-    /*--------------------
-            Constructor
-     --------------------*/
-
-    //Basic gameObject
-    public GameObject(Body body) {
-        this.visible = true;
-        this.health = 100;
-        this.invincible = false;
-        this.body = body;
-        //needs animations
-    }
-
-    /*--------------------
-            Methods
-     --------------------*/
-
-    public void damage(float damage) {
-    }
-
-    public void destroy() {
-    }
-
-    public void setAnimationState(String animationState) {
-    }
-
-    public void drawTo(Graphics graphics) {
-    }
+    
+    
+    
 }
