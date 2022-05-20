0359053d31a52208dca8de4a82fe516acc0b83b8
Josel <jsaiz@cifpfbmoll.eu>
2022-05-20 18:50:57 +0200

    CLI

---
+++ Game/src/PruebasRasteroidFisicas/pruebas_rasteroid/GameObject.java
@@ -0,0 +1,32 @@
+package PruebasRasteroidFisicas.pruebas_rasteroid;
+
+public class GameObject {
+    private Body staticBody;
+    private DynamicBody dynamicBody;
+    
+    public GameObject(DynamicBody dynamicBody){
+        this.dynamicBody = dynamicBody;
+    }
+    
+    public GameObject(Body staticBody){
+        this.staticBody = staticBody;
+    }
+
+    public Body getStaticBody() {
+        return staticBody;
+    }
+
+    public void setStaticBody(Body staticBody) {
+        this.staticBody = staticBody;
+    }
+
+    public DynamicBody getDynamicBody() {
+        return dynamicBody;
+    }
+
+    public void setDynamicBody(DynamicBody dynamicBody) {
+        this.dynamicBody = dynamicBody;
+    }
+    
+    
+}
---
+++ Game/src/View/test/Objects/PlayerModels/PlayerModel.java
@@ -0,0 +1,34 @@
+package View.test.Objects.PlayerModels;
+
+import View.test.Objects.Animation;
+import View.test.Objects.MachineState;
+import View.test.Resources.Sprite;
+
+import java.util.List;
+
+public abstract class PlayerModel {
+
+  protected List<MachineState> machine_states;
+
+  public PlayerModel()
+  {
+    load_states();
+  }
+
+  public abstract void load_states();
+
+  public List<MachineState> get_machine_states()
+  {
+    return machine_states;
+  }
+
+  public void load_sprite_in_range(Animation anim, String path, int sprite_number, int width, int height)
+  {
+    Sprite[] sprites = new Sprite[sprite_number];
+    for (int i = 1; i <= sprite_number; i++) {
+      sprites[i-1] = new Sprite((path+i+".png")).load().resize(width, height);
+    }
+    anim.set_sprite_list(sprites);
+  }
+
+}
