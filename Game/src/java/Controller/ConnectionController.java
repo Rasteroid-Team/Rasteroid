d1aa70dbd529c280bf3afde66c4da4b19b6d5b1f
Sandra <slopez@cifpfbmoll.eu>
2022-06-19 12:11:17 +0200

    Añadido el contador de Jugadores
    Falta verificar si realmente funciona, en mi caso no puedo porque no tengo más móviles xd

--- Game/src/java/Controller/ConnectionController.java
+++ Game/src/java/Controller/ConnectionController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class ConnectionController implements ConnectionInterface {
 
+    int contadorJugadores;
     CommunicationController comController;
     ScreenConnectionController screenConnController;
     PlayerConnectionController playerConnController;
@@ -81,7 +82,9 @@ public class ConnectionController implements ConnectionInterface {
                 screenConnController.receiveShoot(packet);
             }
             case 156 -> {
+                contadorJugadores += 1;
                 System.out.println("Modelo recibido");
+                System.out.println("Jugadores: " + contadorJugadores);
                 playerConnController.setPlayerModel(packet.getObject().toString(), packet.getSourceID());
             }
         }
